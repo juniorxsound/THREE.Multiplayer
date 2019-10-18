9136419f28f7dc82edab1a8013dd929c08651a57
shorookMS <shorook.m.saleh@gmail.com>
2019-10-18 16:44:40 +0300

    added prop change size

--- src/scene.js
+++ src/scene.js
@@ -3,11 +3,12 @@ import * as THREE from "three";
 
 import FirstPersonControls from "./fpscontrols";
 FirstPersonControls(THREE);
-// import OrbitControls from "three-orbit-controls";
-// OrbitControls(THREE);
+
 // Event emitter implementation for ES6
 import EventEmitter from "event-emitter-es6";
 import OrbitControls from "three-orbitcontrols";
+import * as dat from "dat.gui";
+
 // OrbitControls(THREE);
 class Scene extends EventEmitter {
   constructor(
@@ -62,17 +63,143 @@ class Scene extends EventEmitter {
     domElement.append(this.renderer.domElement);
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.update();
-    var geometry = new THREE.SphereGeometry(5, 32, 32);
-    var material = new THREE.MeshBasicMaterial({
-      color: 0xffff00
-      // opacity: 0.1,
-      // blending: THREE.NormalBlending,
-      // depthTest: true
-    });
-    var sphere = new THREE.Mesh(geometry, material);
+    // this.controls.target.set(0, 5, 0);
 
-    this.scene.add(sphere);
+    this.controls.update();
+    var geometry = new THREE.SphereGeometry(5, 15, 15);
+
+    // var material = new THREE.MeshDepthMaterial({
+    //   opacity: 0.5,
+    //   blending: THREE.NormalBlending,
+    //   depthTest: true
+    // });
+
+    var material = new THREE.MeshPhongMaterial({
+      color: 0x404040
+    });
+    this.sphere = new THREE.Mesh(geometry, material);
+
+    this.scene.add(this.sphere);
+
+    // Options to be added to the GUI
+
+    this.options = {
+      velx: 0,
+      vely: 0.01,
+      camera: {
+        speed: 0.0001
+      },
+      stop: function() {
+        this.velx = 0;
+        this.vely = 0;
+      },
+      reset: function() {
+        this.velx = 0.1;
+        this.vely = 0.1;
+        camera.position.z = 75;
+        camera.position.x = 0;
+        camera.position.y = 0;
+        this.sphere.scale.x = 1;
+        this.sphere.scale.y = 1;
+        this.sphere.scale.z = 1;
+        this.sphere.material.wireframe = true;
+      }
+    };
+    var gui = new dat.GUI();
+
+    var cam = gui.addFolder("Camera");
+    // cam.add(this.options.camera, "speed", 0, 0.001).listen();
+    cam.add(this.camera.position, "y", 0, 100).listen();
+    cam.open();
+
+    var velocity = gui.addFolder("Velocity");
+    // velocity
+    //   .add(this.options, "velx", -0.2, 0.2)
+    //   .name("X")
+    //   .listen();
+    velocity
+      .add(this.options, "vely", -0.2, 0.2)
+      .name("Y")
+      .listen();
+    velocity.open();
+
+    var box = gui.addFolder("Cube");
+    var testVal = box.add(this.sphere.scale, "x", 0, 3).name("D");
+    // console.log("testVal", testVal.getValue());
+    testVal.onChange(t => {
+      this.sphere.scale.y = t;
+      this.sphere.scale.z = t;
+
+      // console.log(t);
+    });
+    testVal.listen();
+
+    // box
+    //   .add(this.sphere.scale, "y", 0, 3)
+    //   .name("Height")
+    //   .listen();
+    // box
+    //   .add(this.sphere.scale, "z", 0, 3)
+    //   .name("Length")
+    //   .listen();
+    box.add(this.sphere.material, "wireframe").listen();
+    box.open();
+
+    gui.add(this.options, "stop");
+    gui.add(this.options, "reset");
+
+    var light = new THREE.AmbientLight(0xffffff);
+    {
+      const planeSize = 40;
+
+      const loader = new THREE.TextureLoader();
+      const texture = loader.load(
+        "https://threejsfundamentals.org/threejs/resources/images/checker.png"
+      );
+      texture.wrapS = THREE.RepeatWrapping;
+      texture.wrapT = THREE.RepeatWrapping;
+      texture.magFilter = THREE.NearestFilter;
+      const repeats = planeSize / 2;
+      texture.repeat.set(repeats, repeats);
+
+      const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+      const planeMat = new THREE.MeshPhongMaterial({
+        map: texture,
+        side: THREE.DoubleSide
+      });
+      const mesh = new THREE.Mesh(planeGeo, planeMat);
+      mesh.rotation.x = Math.PI * -0.5;
+      this.scene.add(mesh);
+    }
+
+    // {
+    //   const skyColor = 0xb1e1ff; // light blue
+    //   const groundColor = 0xb97a20; // brownish orange
+    //   const intensity = 1;
+    //   const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+
+    //   this.scene.add(light);
+    // }
+
+    {
+      const color = 0xffffff;
+      const intensity = 1;
+      const light = new THREE.DirectionalLight(color, intensity);
+      light.position.set(0, 10, 0);
+      light.target.position.set(-8, 0, 0);
+      this.scene.add(light);
+      this.scene.add(light.target);
+    }
+
+    {
+      const color = 0xffffff;
+      const intensity = 1;
+      const light = new THREE.SpotLight(color);
+      light.position.set(0, -100, 0);
+      // light.target.position.set(-8, 0, 0);
+      this.scene.add(light);
+      // this.scene.add(light.target);
+    }
     // var material = new THREE.MeshDepthMaterial({
     //   opacity: 0.1,
     //   blending: THREE.NormalBlending,
@@ -128,6 +255,9 @@ class Scene extends EventEmitter {
     this.controls.update();
     // this.controls.update(this.clock.getDelta());
     // this.controls.target = new THREE.Vector3(0, 0, 0);
+    this.sphere.rotation.x += this.options.velx;
+    this.sphere.rotation.y += this.options.vely;
+
     this.render();
   }
 
